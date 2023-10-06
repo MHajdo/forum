cee42ca394c7c0404ad3bebc70e1ea9528b6fc4f
Matej Hajdinjak <hajdinjak.matej@gmail.com>
2023-10-06 22:24:43 +0200

    Started implementing login and sign-up page

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from sqla_wrapper import SQLAlchemy
 from datetime import datetime
 from essential_generators import DocumentGenerator
 from random import randint
+import bcrypt
 
 db = SQLAlchemy('sqlite:///my_db.sqlite')
 
@@ -63,14 +64,27 @@ def messages():
     return 'posti delajo'
 
 
-@app.route('/users', methods=['GET', 'POST'])
-def users():
+@app.route('/sign-up', methods=['GET', 'POST'])
+def user_sign_up():
     if request.method == 'GET':
         all_users = db.query(User).all()
 
-        return render_template('users.html', users=all_users)
+        return render_template('sign-up.html', users=all_users)
     else:
         return 'post handler'
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def user_login():
+    if request.method == 'GET':
+        all_users = db.query(User).all()
+
+        return render_template('login.html', users=all_users)
+    else:
+        username = request.form.get('username')
+        hashed_password = bcrypt.hashpw(request.form.get('password').encode(), bcrypt.gensalt())
+        user = db.query(User).filter_by(username=username, password_hash=hashed_password).all()
+        return f'user: {user}<br> plain-tex pass: {request.form.get("password")}, <br> hashed_pass: {hashed_password}, <br> user_name: {user}'
 
 
 if __name__ == '__main__':
