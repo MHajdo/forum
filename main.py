6ca74478d80f8b07e83e4fbb1c986fddbc2f3c10
Matej Hajdinjak <hajdinjak.matej@gmail.com>
2023-10-10 21:54:12 +0200

    handlers for new templates

--- main.py
+++ main.py
@@ -1,81 +1,79 @@
-from flask import Flask, render_template, request, make_response
-from sqla_wrapper import SQLAlchemy
+from flask import Flask, render_template, request, make_response, redirect, url_for, abort
 from datetime import datetime
 from essential_generators import DocumentGenerator
 from random import randint
+from models import Users, Topics, Messages, db
 import hashlib
 import uuid
 
-db = SQLAlchemy('sqlite:///my_db.sqlite')
-
-class User(db.Model):
-    user_id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String)
-    email = db.Column(db.String)
-    password_hash = db.Column(db.String)
-    session_token = db.Column(db.String)
-
-class Topics(db.Model):
-    topic_id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String)
-    description = db.Column(db.String)
-    created_at = db.Column(db.String)
-    user_id = db.Column(db.Integer)
-
-class Messages(db.Model):
-    message_id = db.Column(db.Integer, primary_key=True)
-    body = db.Column(db.String)
-    created_at = db.Column(db.String)
-    user_id = db.Column(db.Integer)
-    topic_id = db.Column(db.Integer)
-
-db.create_all()
 
 app = Flask(__name__)
+db.create_all()
+
+
+def get_user(session_id=None):
+    if not session_id:
+        session_id = request.cookies.get('session_token')
+    return db.query(Users).filter_by(session_token=session_id).first()
 
 
 @app.route('/')
-def home():
-    user = None
-    session_id = request.cookies.get('session_token')
-    if session_id:
-        user = db.query(User).filter_by(session_token=session_id).first()
-
-    if user:
-        message=f'logged in with user "{user.username}"'
-    return render_template('index.html', msg=message)
+def index():
+    user = get_user()
+    return render_template('index.html', user=user)
 
 
-@app.route('/topics')
+@app.route('/topics', methods=['GET', 'POST'])
 def topics():
-    generator = DocumentGenerator()
-    title = generator.sentence()
-    desc = generator.sentence()
-    topic = Topics(title=title, description=desc, created_at=datetime.now(), user_id=1)
-    db.add(topic)
-    db.commit()
-
-    return 'works'
+    topic = db.query(Topics).all()
+    return render_template('topics.html', topic=topic)
 
 
-@app.route('/posts', methods=['GET', 'POST'])
-def messages():
-    generator = DocumentGenerator()
-    for i in range(50):
-        body = generator.gen_sentence(30, 300)
-        topic_id = randint(1, 5)
-        user_id = randint(1, 10)
-        message = Messages(body=body, created_at=datetime.now(), user_id=user_id, topic_id=topic_id)
-        db.add(message)
-        db.commit()
+@app.route('/topics/<topic_id>', methods=['GET'])
+def topics_topic_detail(topic_id):
+    try:
+        topic_id = int(topic_id)
+    except ValueError:
+        return abort(404)
 
-    return 'posti delajo'
+    topic = db.query(Topics).filter_by(id=int(topic_id)).first()
+    return render_template('topic.html', topic=topic)
+
+
+@app.route('/topics/new-topic', methods=['GET', 'POST'])
+def topics_new_topic():
+    if request.method == 'GET':
+        return render_template('topics-new-topic.html')
+    else:
+        title = request.form.get('topic-title')
+        text = request.form.get('topic-text')
+
+        user = get_user()
+        if not user:
+            return redirect(url_for('login'))
+
+        topic = Topics.create(title=title, text=text, author=user)
+        return redirect(url_for('topics'))
+
+
+#@app.route('/posts', methods=['GET', 'POST'])
+#def messages():
+#    generator = DocumentGenerator()
+#    for i in range(50):
+#        body = generator.gen_sentence(30, 300)
+#        topic_id = randint(1, 5)
+#        user_id = randint(1, 10)
+#        message = Messages(body=body, created_at=datetime.now(), user_id=user_id, topic_id=topic_id)
+#        db.add(message)
+#        db.commit()
+#
+#    return 'posti delajo'
 
 
 @app.route('/sign-up', methods=['GET', 'POST'])
 def user_sign_up():
     if request.method == 'GET':
-        all_users = db.query(User).all()
+        all_users = db.query(Users).all()
 
         return render_template('sign-up.html', users=all_users)
     else:
@@ -83,7 +81,7 @@ def user_sign_up():
         email = request.form.get('email')
         hashed_password = hashlib.sha256(request.form.get('password').encode()).hexdigest()
         session_token = str(uuid.uuid4())
-        user = User(username=username, email=email, password_hash=hashed_password, session_token=session_token)
+        user = Users(username=username, email=email, password_hash=hashed_password, session_token=session_token)
         db.add(user)
         db.commit()
 
@@ -95,19 +93,19 @@ def user_sign_up():
 @app.route('/login', methods=['GET', 'POST'])
 def user_login():
     if request.method == 'GET':
-        all_users = db.query(User).all()
+        all_users = db.query(Users).all()
 
         return render_template('login.html', users=all_users)
     else:
         username = request.form.get('username')
         hashed_password = hashlib.sha256(request.form.get('password').encode()).hexdigest()
-        user = db.query(User).filter_by(username=username, password_hash=hashed_password).first()
+        user = db.query(Users).filter_by(username=username, password_hash=hashed_password).first()
         if user:
             response = make_response('login successful')
             response.set_cookie('session_token', user.session_token)
             return response
         else:
-            return 'login failed'
+            return redirect(url_for('user_sign_up'))
 
 
 if __name__ == '__main__':
